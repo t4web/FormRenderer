e1e07dbd5c55110d95e83a92e702f906b05626a3
maxgu <maxgu@t4web.com.ua>
2015-04-24 16:20:00 +0300

    add Module.php, config.php

---
+++ config/module.config.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+        'display_exceptions' => true,
+        'display_not_found_reason' => true,
+    ),
+
+);
