15f43374a86c49e45f5f98807873486b63a773ef
maxgu <maxgu@t4web.com.ua>
2015-10-11 11:14:25 +0300

    add file test

--- tests/T4webFormRendererTest/FactoryTest.php
+++ tests/T4webFormRendererTest/FactoryTest.php
@@ -280,4 +280,55 @@ class FactoryTest extends \PHPUnit_Framework_TestCase
             preg_replace('/\s+/', ' ', $rawHtml)
         );
     }
+
+    public function testFromRenderWithFile()
+    {
+        $formConfig = [
+            'template' => 't4web-form-renderer/element/form',
+            'children' => [
+                'file' => [
+                    'template' => 't4web-form-renderer/element/file',
+                    'variables' => [
+                        'label' => 'File'
+                    ]
+                ],
+            ],
+            'variables' => [
+                'action' => '/admin/news/create',
+                'cancelLink' => '/admin/list'
+            ],
+        ];
+
+        $factory = new Factory();
+        $form = $factory->create($formConfig);
+
+        $form->setMessages([
+            'file' => [
+                "This field is required"
+            ]
+        ]);
+
+        $rawHtml = $this->renderer->render($form);
+
+        $this->assertEquals(
+            preg_replace(
+                '/\s+/',
+                ' ',
+                '<form method="post" action="/admin/news/create">
+                    <div class="box-body">
+                        <div class="form-group has-error">
+                            <label class="control-label">File</label>
+                            <input type="file" name="file" class="form-control">
+                            <p class="help-block">This field is required</p>
+                        </div>
+                    </div>
+                    <div class="box-footer">
+                        <button type="submit" class="btn btn-success" id="submit-btn">Submit</button>
+                        <a class="btn btn-default" href="/admin/list">Cancel</a>
+                    </div>
+                </form>'
+            ),
+            preg_replace('/\s+/', ' ', $rawHtml)
+        );
+    }
 }
